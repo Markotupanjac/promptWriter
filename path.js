4c90abe13335b664b270cbba2c75d2b26f99d8f3
Markotupanjac <markotupanjac@gmail.com>
2014-09-06 21:05:31 -0500

    Progression

--- path.js
+++ path.js
@@ -1,3 +1,25 @@
+var Comedy = [
+"Kanye West is one of the four Kanye's. The other three being Kanye North, Kanye South, and Kanye East. What happens when they all meet.",
+"A Mime is a witness to a murder, explain how he tells the police of what he saw. Remember: Mimes cannot speak.",
+];
+var Horror = [
+	"A wife kills her husband, mae the reader sympathize with both characters.",
+	"A suicide hotline operator realizes that the person he's talking down is a wanted serial killer",
+	"You've just been selected for jury duty. As the trial begins, the opening prosecutor details a gruesome murder that you instantly recognize..because you committed it and the defendant is being framed.",
+	"A crazy man get admitted to a mental institution where he meets another crazy man who hears the same voices as him. Write of the man's expierence.",
+	"You are legally allowed to commit murder once, but you must fill out the proper paperwork and your proposed victim will be notified of your intentions",
+];
+var Thriller = [
+	"You are legally allowed to commit murder once, but you must fill out the proper paperwork and your proposed victim will be notified of your intentions",
+	"You've just been selected for jury duty. As the trial begins, the opening prosecutor details a gruesome murder that you instantly recognize..because you committed it and the defendant is being framed.",
+	"Harry Potter is sitting at home sixty years after the battle of Hogwarts, an old man now, when suddenly his scar hurts.",
+	"An old genie grants you three wishes. After granting your first two, you tell him the third. He is horrified, and begs you to reconsider. Describe The entire event.",
+];
+var Drama = [
+"A wife kills her husband, mae the reader sympathize with both characters.",
+"A crazy man get admitted to a mental institution where he meets another crazy man who hears the same voices as him. Write of the man's expierence.",
+"An old genie grants you three wishes. After granting your first two, you tell him the third. He is horrified, and begs you to reconsider. Describe The entire event.",
+];
 var beginBut = document.getElementById("begin");
 var title = document.getElementById("Title");
 var subtitle = document.getElementById("subTitle");
@@ -43,7 +65,8 @@ count4++;
 localStorage.setItem("box4Num", count4);
 
 })
-	function q2(){
+
+function q2(){
 		if(one.innerHTML == 'Happy'){
 		title.style.visibility = 'true';
 		one.innerHTML = 'Happy';
@@ -73,5 +96,28 @@ localStorage.setItem("box4Num", count4);
 		box3.checked = false;
 		box4.checked = false;
 	}
+	if((count1 + count2 + count3 + count4) + 1 === 3){
+		title.innerHTML = 'If you had to have one of these superpowers, which would it be?';
+		one.innerHTML = "Flying, but you can't wear pants while flying";
+		two.innerHTML = 'Run for your life into an abandoned house';
+		three.innerHTML = 'Teleporting but you randomly teleport once a week';
+		four.innerHTML = 'Invisibility you can only control it in the summer';
+		box1.checked = false;
+		box2.checked = false;
+		box3.checked = false;
+		box4.checked = false;
 	}
+	if((count1 + count2 + count3 + count4) + 1 === 4){
+		title.innerHTML = 'What is your favorite color?';
+		one.innerHTML = "Blue";
+		two.innerHTML = 'Black';
+		three.innerHTML = 'Red';
+		four.innerHTML = 'Green';
+		box1.checked = false;
+		box2.checked = false;
+		box3.checked = false;
+		box4.checked = false;
+	} 
+	}
+
 	
